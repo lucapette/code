91e2f1139fe936340863985988c9fbe5da2fa166
lucapette <ciao@lucapette.me>
2026-02-13 17:36:54 +0100

    Move drafts preprocessor to dedicated folder

--- web/lucapette/.eleventy.js
+++ web/lucapette/.eleventy.js
@@ -3,7 +3,7 @@ import { eq } from "./src/_filters/comparison.js";
 import { formatDate } from "./src/_filters/date.js";
 import { writings, writingsByYear, favouriteWritings } from "./src/_collections/writings.js";
 import { reading, readingByYear } from "./src/_collections/reading.js";
-import { drafts } from "./src/_collections/drafts.js";
+import { drafts } from "./src/_preprocessors/drafts.js";
 import lucideShortcode from "./src/_shortcodes/lucide.js";
 
 export default function (eleventyConfig) {
