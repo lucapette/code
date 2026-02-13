0e95639204530534b63fc14a3ee49d7a7c2b4f9a
lucapette <ciao@lucapette.me>
2026-02-13 21:10:12 +0100

    Add line numbers and copy button to code blocks

--- web/lucapette/src/assets/js/main.js
+++ web/lucapette/src/assets/js/main.js
@@ -30,6 +30,60 @@ toggle.addEventListener("click", function (e) {
   );
 });
 
+document.querySelectorAll("pre[class*='language-']").forEach((pre) => {
+  // Add line numbers
+  const code = pre.querySelector("code");
+  const lines = code.textContent.split("\n");
+  
+  if (lines.length > 1) {
+    pre.classList.add("line-numbers");
+    const lineNumbersWrapper = document.createElement("span");
+    lineNumbersWrapper.className = "line-numbers-rows";
+    lineNumbersWrapper.setAttribute("aria-hidden", "true");
+    
+    lines.forEach(() => {
+      const span = document.createElement("span");
+      lineNumbersWrapper.appendChild(span);
+    });
+    
+    code.appendChild(lineNumbersWrapper);
+  }
+
+  // Add copy button
+  const copyButton = document.createElement("button");
+  copyButton.className = "copy-code";
+  copyButton.textContent = "copy";
+
+  function copyingDone() {
+    copyButton.textContent = "copied!";
+    setTimeout(() => {
+      copyButton.textContent = "copy";
+    }, 2000);
+  }
+
+  copyButton.addEventListener("click", () => {
+    const code = pre.querySelector("code");
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(code.textContent);
+      copyingDone();
+    } else {
+      const range = document.createRange();
+      range.selectNodeContents(code);
+      const selection = window.getSelection();
+      selection.removeAllRanges();
+      selection.addRange(range);
+      try {
+        document.execCommand("copy");
+        copyingDone();
+      } catch (e) {}
+      selection.removeAllRanges(range);
+    }
+  });
+
+  pre.style.position = "relative";
+  pre.appendChild(copyButton);
+});
+
 console.info(
   " 👋👋👋\n Grab the code here: https://github.com/lucapette/web/tree/main/main\n 👋👋👋",
 );
